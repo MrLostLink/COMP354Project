633ddcd10cfb60226c07b0cf4beee180357e2952
KendyCompSci <kjmaster3@hotmail.com>
2016-10-31 21:21:18 -0400

    Add files via upload

---
+++ CalculateMovingAverage.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by kjmas on 10/27/2016.
+ */
+public class CalculateMovingAverage {
+
+    BufferedReader sampleData = null;
+
+    String csv="E:\\Comp354\\Comp354project\\Sampledata.csv";
+
+        try
+    {
+        //reading from the CSV file provided
+        String line;
+        sampleData = new BufferedReader(new FileReader("E:\\Comp354\\Comp354project\\Sampledata.csv"));
+    }
+}
